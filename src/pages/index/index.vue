81323320b71b7a69f3fae7c864451bc0ca2fbad6
liujianfeng <masoneastc@gmail.com>
2025-04-02 16:53:14 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="content">
+    <view class="header">
+      <uni-icons type="search" size="30"> </uni-icons>
+    </view>
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const title = ref("a");
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.header {
+  height: 150rpx;
+  width: 100%;
+  background-color: #8f8f94;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
