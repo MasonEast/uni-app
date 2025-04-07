b004be1548cc83c59db6acb798eb4935a5ccb332
liujianfeng <masoneastc@gmail.com>
2025-04-07 18:14:15 +0800

    feat

--- src/pages/partner/components/detail.vue
+++ src/pages/partner/components/detail.vue
@@ -1,7 +1,7 @@
 <template>
-  <view class="content">
-    <view>
-      <image class="img_icon" src="@/static/png/a-car5.png" />
+  <view class="partner_detail">
+    <view class="user">
+      <image class="img" src="@/static/png/a-car5.png" />
       <view>风五岁</view>
     </view>
     <view> 这个周六，想去逛街，看看春季的衣服，一起吃饭，看电影，求组队 </view>
@@ -31,30 +31,17 @@ export default {
 };
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+<style lang="scss" scoped>
+.partner_detail {
+  .user {
+    margin: 10px;
+    display: flex;
+    justify-content: space-between;
+    .img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+    }
+  }
 }
 </style>
